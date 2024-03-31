70805876d7cb0b67850bcdaca196d25234e4bf2a
CookedBear <1138165028@qq.com>
2024-03-31 06:15:35 -0700

    Finish task3

--- vel_pkg/scripts/vel_ctrl_node.py
+++ vel_pkg/scripts/vel_ctrl_node.py
@@ -1,18 +1,87 @@
 #!/usr/bin/env python
-# coding=utf-8
+# -*- coding: utf-8 -*-
 import rospy
 from geometry_msgs.msg import Twist
-if __name__ == "__main__":
-    rospy.init_node("vel_ctrl_node")
-    # 发布速度控制话题
-    vel_pub = rospy.Publisher("cmd_vel",Twist,queue_size=10)
-    # 构建速度消息包并赋值
-    msg = Twist()
-    msg.linear.x = 0.1
-    # msg.angular.z =0.1
-    # 构建发送频率对象
-    rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        rospy.loginfo("发送一个速度消息包")
-        vel_pub.publish(msg)
-        rate.sleep()
+import sys, select, termios, tty
+
+msg = """
+控制小车！
+---------------------------
+移动指令:
+   q    w    e
+   a    s    d
+空格键: 刹车
+
+CTRL-C 退出
+"""
+
+# 键盘字符映射
+moveBindings = {
+    'w':(1,0,0,0),
+    's':(-1,0,0,0),
+    'a':(0,0,0,1),
+    'd':(0,0,0,-1),
+    'q':(0,0,1,0),
+    'e':(0,0,-1,0),
+    ' ':(0,0,0,0),
+}
+
+def getKey():
+    tty.setraw(sys.stdin.fileno())
+    select.select([sys.stdin], [], [], 0)
+    key = sys.stdin.read(1)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+def vels(speed,turn):
+    return "当前速度: %s\n当前转向: %s " % (speed,turn)
+
+if __name__=="__main__":
+    settings = termios.tcgetattr(sys.stdin)
+    
+    rospy.init_node('keyboard_control')
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
+    
+    speed = 0.5
+    turn = 1.0
+    
+    try:
+        print(msg)
+        while(1):
+            key = getKey()
+            if key in moveBindings.keys():
+                x = moveBindings[key][0]
+                y = moveBindings[key][1]
+                z = moveBindings[key][2]
+                th = moveBindings[key][3]
+            else:
+                x = 0
+                y = 0
+                z = 0
+                th = 0
+                if (key == '\x03'):
+                    break
+
+            twist = Twist()
+            twist.linear.x = x*speed
+            twist.linear.y = y*speed
+            twist.linear.z = z*speed
+            twist.angular.x = 0
+            twist.angular.y = 0
+            twist.angular.z = th*turn
+            pub.publish(twist)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        twist = Twist()
+        twist.linear.x = 0
+        twist.linear.y = 0
+        twist.linear.z = 0
+        twist.angular.x = 0
+        twist.angular.y = 0
+        twist.angular.z = 0
+        pub.publish(twist)
+
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
